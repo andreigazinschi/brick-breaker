fe39169dc7072f53633c664a879b60f81f898f1a
andreigazinschi <63117209+andreigazinschi@users.noreply.github.com>
2020-04-13 11:27:33 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,24 +2,83 @@ namespace SpriteKind {
     export const Edge = SpriteKind.create()
     export const Ball = SpriteKind.create()
     export const top = SpriteKind.create()
+    export const brick = SpriteKind.create()
 }
 sprites.onOverlap(SpriteKind.Ball, SpriteKind.Player, function (sprite, otherSprite) {
-    sprite.setVelocity((sprite.x - otherSprite.x) * 3, -1 * sprite.vy)
+    sprite.setVelocity((sprite.x - otherSprite.x) * 4, -1 * sprite.vy)
     if (sprite.vy > -150) {
         sprite.vy += -5
     }
 })
-function buildSetBricks () {
-    for (let index = 0; index <= 4; index++) {
-    	
+function createBrick (x: number, y: number) {
+    randNum = Math.randomRange(0, 2)
+    if (randNum == 0) {
+        brick = sprites.create(img`
+f f f f f f f f f f f f f f f f 
+f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+f 2 2 2 2 2 2 2 2 2 2 2 2 2 2 f 
+f f f f f f f f f f f f f f f f 
+`, SpriteKind.brick)
+    } else if (randNum == 1) {
+        brick = sprites.create(img`
+f f f f f f f f f f f f f f f f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f 
+f f f f f f f f f f f f f f f f 
+`, SpriteKind.brick)
+    } else {
+        brick = sprites.create(img`
+f f f f f f f f f f f f f f f f 
+f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+f 5 5 5 5 5 5 5 5 5 5 5 5 5 5 f 
+f f f f f f f f f f f f f f f f 
+`, SpriteKind.brick)
     }
+    brick.setPosition(x, y)
+    numBricks += 1
 }
 sprites.onOverlap(SpriteKind.Ball, SpriteKind.Edge, function (sprite, otherSprite) {
     sprite.setVelocity(-1 * sprite.vx, sprite.vy)
 })
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    console.log(convertToText(numBricks))
+})
 sprites.onOverlap(SpriteKind.Ball, SpriteKind.top, function (sprite, otherSprite) {
     sprite.setVelocity(sprite.vx, -1 * sprite.vy)
 })
+sprites.onOverlap(SpriteKind.Ball, SpriteKind.brick, function (sprite, otherSprite) {
+    info.changeScoreBy(15)
+    otherSprite.destroy(effects.disintegrate, 200)
+    sprite.setVelocity(sprite.vx, -1 * sprite.vy)
+    numBricks += -1
+})
+function buildSetBricks () {
+    for (let index = 0; index <= 6; index++) {
+        for (let index2 = 0; index2 < 4; index2++) {
+            createBrick(index * 16 + 32, col * 8 + 24)
+            col += 1
+        }
+        col = 0
+    }
+}
+let brick: Sprite = null
+let randNum = 0
+let numBricks = 0
+let col = 0
+info.setScore(0)
+info.setLife(3)
 let startBallVar = 0
 scene.setBackgroundColor(8)
 let Paddle = sprites.create(img`
@@ -287,6 +346,9 @@ let BallVar = sprites.create(img`
 1 1 1 1 
 . 1 1 . 
 `, SpriteKind.Ball)
+col = 0
+numBricks = 0
+buildSetBricks()
 game.onUpdate(function () {
     if (startBallVar == 0) {
         BallVar.setPosition(Paddle.x, 105)
@@ -301,5 +363,17 @@ game.onUpdate(function () {
     }
     if (BallVar.y > 115) {
         startBallVar = 0
+        info.changeLifeBy(-1)
+    }
+})
+forever(function () {
+    if (numBricks <= 0) {
+        numBricks = 0
+        startBallVar = 0
+        effects.confetti.startScreenEffect()
+        pause(2000)
+        effects.confetti.endScreenEffect()
+        buildSetBricks()
+        info.changeScoreBy(100)
     }
 })
