df86e4e1446a5466305e248f4ea85460e22a37b9
andreigazinschi <63117209+andreigazinschi@users.noreply.github.com>
2020-04-13 11:28:50 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,6 +10,30 @@ sprites.onOverlap(SpriteKind.Ball, SpriteKind.Player, function (sprite, otherSpr
         sprite.vy += -5
     }
 })
+sprites.onOverlap(SpriteKind.Ball, SpriteKind.Edge, function (sprite, otherSprite) {
+    sprite.setVelocity(-1 * sprite.vx, sprite.vy)
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    console.log(convertToText(numBricks))
+})
+sprites.onOverlap(SpriteKind.Ball, SpriteKind.top, function (sprite, otherSprite) {
+    sprite.setVelocity(sprite.vx, -1 * sprite.vy)
+})
+sprites.onOverlap(SpriteKind.Ball, SpriteKind.brick, function (sprite, otherSprite) {
+    info.changeScoreBy(15)
+    otherSprite.destroy(effects.disintegrate, 200)
+    sprite.setVelocity(sprite.vx, -1 * sprite.vy)
+    numBricks += -1
+})
+function buildSetBricks () {
+    for (let index = 0; index <= 6; index++) {
+        for (let index2 = 0; index2 < 4; index2++) {
+            createBrick(index * 16 + 32, col * 8 + 24)
+            col += 1
+        }
+        col = 0
+    }
+}
 function createBrick (x: number, y: number) {
     randNum = Math.randomRange(0, 2)
     if (randNum == 0) {
@@ -48,30 +72,6 @@ f f f f f f f f f f f f f f f f
     }
     brick.setPosition(x, y)
     numBricks += 1
-}
-sprites.onOverlap(SpriteKind.Ball, SpriteKind.Edge, function (sprite, otherSprite) {
-    sprite.setVelocity(-1 * sprite.vx, sprite.vy)
-})
-controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    console.log(convertToText(numBricks))
-})
-sprites.onOverlap(SpriteKind.Ball, SpriteKind.top, function (sprite, otherSprite) {
-    sprite.setVelocity(sprite.vx, -1 * sprite.vy)
-})
-sprites.onOverlap(SpriteKind.Ball, SpriteKind.brick, function (sprite, otherSprite) {
-    info.changeScoreBy(15)
-    otherSprite.destroy(effects.disintegrate, 200)
-    sprite.setVelocity(sprite.vx, -1 * sprite.vy)
-    numBricks += -1
-})
-function buildSetBricks () {
-    for (let index = 0; index <= 6; index++) {
-        for (let index2 = 0; index2 < 4; index2++) {
-            createBrick(index * 16 + 32, col * 8 + 24)
-            col += 1
-        }
-        col = 0
-    }
 }
 let brick: Sprite = null
 let randNum = 0
